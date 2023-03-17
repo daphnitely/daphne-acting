2e17b39e0d7c3b6b01a4b50b0915058028eaed7f
Daphne Liu <79234232+daphnitely@users.noreply.github.com>
2023-03-17 13:08:24 -0700

    Configure resume upload

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("resume.pdf");
 
   eleventyConfig.addPassthroughCopy({
     "node_modules/normalize.css/normalize.css": "assets/css/normalize.css",
